9b39a72f88a2789e735ea6ca06f7a69ec7c6240d
Sjef van Leeuwen <36629650+sjefvanleeuwen@users.noreply.github.com>
2018-03-26 00:36:27 +0000

    test passing smart identity contract deployments

--- src/Wigo4It.Blockchain.Core/Wigo4It.Blockchain.Core.Tests/UnitTests/ContractDeploymentTests.cs
+++ src/Wigo4It.Blockchain.Core/Wigo4It.Blockchain.Core.Tests/UnitTests/ContractDeploymentTests.cs
@@ -8,7 +8,97 @@ namespace Wigo4It.Blockchain.Core.Tests.UnitTests
 {
     [TestClass]
     public class ContractDeplomentTests
-    {
+    { 
+        [TestMethod]
+        public void ShouldDeploySmartIdentityRegistry()
+        {
+            using (var testrpcRunner = new TestRPCEmbeddedRunner())
+            {
+                try
+                {
+                    testrpcRunner.RedirectOuputToDebugWindow = true;
+                    testrpcRunner.StartTestRPC();
+                    var web3 = new Web3();
+                    var addressFrom = (web3.Eth.Accounts.SendRequestAsync()).Result[0];
+                    // Deploy the contract and add the multiplier to the constructor
+                    var transactionHash =
+                        SmartIdentityRegistryService.DeployContractAsync(web3, addressFrom, new HexBigInteger(900000)).Result;
+                    Assert.IsNotNull(transactionHash);
+                    var receipt = web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash).Result;
+                    Assert.IsNotNull(receipt);
+                    var s  = new SmartIdentityRegistryService(web3, addressFrom);
+                }
+                finally
+                {
+                    testrpcRunner.StopTestRPC();
+                }
+            }
+
+        }
+
+        [TestMethod]
+        public void ShouldDeploySmartIdentityMigrations()
+        {
+            using (var testrpcRunner = new TestRPCEmbeddedRunner())
+            {
+                try
+                {
+                    testrpcRunner.RedirectOuputToDebugWindow = true;
+                    testrpcRunner.StartTestRPC();
+                    var web3 = new Web3();
+                    var addressFrom = (web3.Eth.Accounts.SendRequestAsync()).Result[0];
+                    // Deploy the contract and add the multiplier to the constructor
+                    var transactionHash =
+                        MigrationsService.DeployContractAsync(web3, addressFrom, new HexBigInteger(900000)).Result;
+                    Assert.IsNotNull(transactionHash);
+                    var receipt = web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash).Result;
+                    Assert.IsNotNull(receipt);
+                }
+                finally
+                {
+                    testrpcRunner.StopTestRPC();
+                }
+            }
+
+        }
+    
+        [TestMethod]
+        public void ShouldDeploySmartIdentity()
+        {
+            using (var testrpcRunner = new TestRPCEmbeddedRunner())
+            {
+                try
+                {
+
+                    testrpcRunner.RedirectOuputToDebugWindow = true;
+                    testrpcRunner.StartTestRPC();
+                    var web3 = new Web3();
+                    var addresses = (web3.Eth.Accounts.SendRequestAsync()).Result;
+                    var alice = addresses[0];
+                    var retailBank = addresses[1];
+                    var commercialBank = addresses[2];
+
+                    var kycAttributeHash = "0xca02b2202ffaacbd499438ef6d594a48f7a7631b60405ec8f30a0d7c096dc3ff";
+                    var endorsementHash = "0xca02b2202ffaacbd499438ef6d594a48f7a7631b60405ec8f30a0d7c096d54d5";
+
+                    //smartIdentity.GetOwnerAsync()
+
+                    // Deploy the contract and add the multiplier to the constructor
+                   var gas= web3.Eth.DeployContract.EstimateGasAsync(SmartIdentityService.ABI, SmartIdentityService.BYTE_CODE,
+                        alice).Result;
+                    var transactionHash =
+                        SmartIdentityService.DeployContractAsync(web3, alice, gas).Result;
+                    Assert.IsNotNull(transactionHash);
+                    var receipt = web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash).Result;
+                    Assert.IsNotNull(receipt);
+                }
+                finally
+                {
+                    testrpcRunner.StopTestRPC();
+                }
+            }
+        }
+
         [TestMethod]
         public void ShouldDeployAContractWithConstructor()
         {
